5f0b1cd9269141331e0ba746f7049a83b7600d55
Elton <seuemail@gmail.com>
2026-05-01 23:39:10 -0300

    fix tela branca

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ def criar_banco():
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return redirect("/produtos")
 
 @app.route("/produtos")
 def produtos():
@@ -190,3 +190,11 @@ def add_fornecedor():
 if __name__ == "__main__":
     criar_banco()
     app.run(debug=True)
+
+    import os
+
+if __name__ == "__main__":
+    criar_banco()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
